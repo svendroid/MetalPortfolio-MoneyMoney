1b8ccf515064bdafc2293cc43c7f6bc4317bd728
Sven Adolph <mail@svenadolph.net>
2021-11-15 19:49:12 +0100

    Migrate extension to goldapi

---
+++ MetalPortfolio.lua
@@ -0,0 +1,110 @@
+-- Inofficial MetalPortfolio Extension for MoneyMoney
+-- Fetches Metal price via goldapi.io API
+-- Returns metal prices as securities
+--
+-- Username: Metal symbol [XAU - Gold, XAG - Silver, XPT - Platinum, XPD - Palladium, DJI - Dow Jones ] comma seperated with number of shares in brackets (Example: "XAU(2),XAG(1.4)")
+-- Password: goldapi API-Key
+
+-- MIT License
+
+-- Original work Copyright (c) 2017 Jacubeit
+-- Modified work Copyright 2020 tobiasdueser
+-- Modified work Copyright 2021 svenadolph
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+WebBanking{
+  version = 1.00,
+  country = "de",
+  description = "Include your metal portfolio in MoneyMoney by providing the metal symbols and amount as username [Example: XAU(2),XAG(1.4)] and a free goldapi API-Key as password.",
+  services= { "MetalPortfolio" }
+}
+
+local symbols
+local connection = Connection()
+local currency = "EUR"
+local token
+
+function SupportsBank (protocol, bankCode)
+  return protocol == ProtocolWebBanking and bankCode == "MetalPortfolio"
+end
+
+function InitializeSession (protocol, bankCode, username, username2, password, username3)
+  symbols = username:gsub("%s+", "")
+  token = password
+end
+
+function ListAccounts (knownAccounts)
+  local account = {
+    name = "MetalPortfolio",
+    accountNumber = "MetalPortfolio",
+    currency = currency,
+    portfolio = true,
+    type = "AccountTypePortfolio"
+  }
+
+  return {account}
+end
+
+function RefreshAccount (account, since)
+  local s = {}
+
+  for metal in string.gmatch(symbols, '([^,]+)') do
+
+    -- Pattern: XAU(1),XAG(1.2)
+    quantity=metal:match("%((%S+)%)")
+    name=metal:match('([^(]+)')
+
+    currentPrice = requestCurrentPrice(name)
+
+    s[#s+1] = {
+      name = name,
+      currency = nil,
+      quantity = quantity,
+      price = currentPrice,
+    }
+
+  end
+
+  return {securities = s}
+end
+
+function EndSession ()
+end
+
+
+-- Query Functions
+function requestCurrentPrice(symbol)
+  headers = {}
+  headers["x-access-token"] = token
+
+  response = connection:request("GET", 
+                                currentPriceRequestUrl(symbol),
+                                nil,
+                                nil,
+                                headers
+                              )
+  json = JSON(response)
+  return json:dictionary()["price"]
+end
+
+-- Helper Functions
+function currentPriceRequestUrl(symbol)
+  return "https://www.goldapi.io/api/" .. symbol .. "/" .. currency
+end
--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-# StockPortfolio for MoneyMoney
-Create a custom portfolio with US stocks and fetch the current stock value via finnhub.io
+# MetalPortfolio for MoneyMoney
+Create a custom portfolio with metals (e.g. gold or silver) and fetch the current value via goldapi.io
 
-![MoneyMoney screenshot with StockPortfolio Balance](screens/StockPortfolio-balance.png)
+![MoneyMoney screenshot with MetalPortfolio Balance](screens/balance.png)
 
 ## Setup
 
-1. Download the extension via the [GitHub releases page](https://github.com/tobiasdueser/MoneyMoney-StockPortfolio/releases/tag/v1.1)
-2. Once downloaded, move `StockPortfolio.lua` to your MoneyMoney Extensions folder.
-3. Create a free [finnhub.io](https://finnhub.io/) account and copy the API-key from [finnhub.io/dashboard](https://finnhub.io/dashboard)
-4. Add a new account with the type `StockPortfolio`
-5. Use the stock symbols comma seperated with the number of shares in brackets as user name. Example: `AAPL(0.7),TSLA(1.5)`
-6. Use the free Finnhub API key as password. Example: `5r8ufep1br56mlvrhj6g`
+1. Download the extension `MetalPortfolio.lue`
+2. Once downloaded, move `MetalPortfolio.lua` to your MoneyMoney Extensions folder.
+3. Create a free [goldapi.io](https://www.goldapi.io/) account and copy the API-key from [https://www.goldapi.io/dashboard](https://www.goldapi.io/dashboard)
+4. Add a new account with the type `MetalPortfolio`
+5. Use the Metal symbols from the goldapi page comma seperated with the number of units in brackets as user name. Example: `XAU(1),XAG(2)`
+6. Use the API key as password. Example: `goldap-xyasdfasdfasdfasdf-io`
 
-![MoneyMoney screenshot with StockPortfolio Setup](screens/StockPortfolio-setup.png)
+![MoneyMoney screenshot with MetalPortfolio Setup](screens/setup.png)
 
-### Adding, editing and removing stocks
-1. Right click on `StockPortfolio` in sidebar
+### Adding, editing and removing items
+1. Right click on `MetalPortfolio` in sidebar
 2. Click `Edit login data...`
 3. Edit and save
 
@@ -23,4 +23,4 @@ Create a custom portfolio with US stocks and fetch the current stock value via f
 Add the buying rates in MoneyMoney via right click on the stock to display the profits.
 
 ### Requirements
-You need at least version 2.4.0 or a beta version of MoneyMoney.
+You need a beta version of MoneyMoney.
